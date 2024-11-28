2676930c23ca535f03da01bbb5bbb3629055c8df
kortin.zhou <kortin1999@outlook.com>
2024-11-28 15:26:50 +0800

    feat: change to function def

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { definePreset } from '@unocss/core'
+import { Preset } from '@unocss/core'
 import { rules } from './rules/index'
 
 export { rules }
@@ -10,10 +10,10 @@ export interface PresetOptions {
   // to be defined
 }
 
-export default definePreset((_options: PresetOptions) => {
+export default function (_options?: PresetOptions): Preset {
 
   return {
     name: '@kortin/unocss-preset',
     rules,
   }
-})
+}
